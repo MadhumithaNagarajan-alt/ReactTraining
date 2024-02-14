1574dce265637c7983abbf3fa0f1d7f762f13ad5
MadhumithaNagarajan-alt <152258960+MadhumithaNagarajan-alt@users.noreply.github.com>
2024-02-14 16:35:17 +0530

    first commit

---
+++ Day9-demo/src/components/Master.js
@@ -0,0 +1,13 @@
+const Master = () => {
+    return (
+        <div className="masterContainer">
+        <div className="masterWrapper">
+       <><div className="HeroTextWrapper">
+            <h1 className="heroText">Order Food Online in Chennai</h1>
+        </div><div className="imageWrapper">
+                <img class="heroImage" src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_1002,h_600/v1678428358/portal/m/seo_web/dweb_header.png" width="501" height="300" alt="food" />
+            </div></>
+            </div></div>
+    )
+}
+export default Master;
